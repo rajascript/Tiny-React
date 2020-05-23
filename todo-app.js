0c7b19a4a8f75eb66daf49d3c2a73087f4c78c95
Raja Raghav <raja_raghav@hotmail.com>
2020-05-23 19:52:28 +0530

    createElement function created

--- todo-app.js
+++ todo-app.js
@@ -1,5 +1,13 @@
-/** @jsx TinyReact.createElement */
+/** @jsx TinyReact.createElement */ //babel replaces () with TinyReact.createElement
 
 /*** Step 1,2,3,4 - createElement */
 
 const root = document.getElementById("root");
+
+let base = (
+  <div>
+    <h1>I am a react element</h1>
+  </div>
+);
+
+console.log(base);
